d5ceaa91f40c22a6e7716a2fd428129c1c1f1026
volry <v.v.ryzhuk@gmail.com>
2024-06-15 09:50:22 +0300

    add indexes

--- task.sql
+++ task.sql
@@ -1,2 +1,12 @@
 # Write your SQL code for the database creation here. Good luck! 
 USE ShopDB; 
+
+
+-- we create an index on the Email column because user everytime type it to enter
+CREATE INDEX Email ON Customers(Email);
+
+-- it helps to faster find orders
+CREATE INDEX Date ON Orders(Date);
+
+-- it also need to speed up product search
+CREATE INDEX Name ON Products(Name);

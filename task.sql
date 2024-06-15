17ff69368f464ef40be91ab3c2eb68d4d8ed11c0
volry <v.v.ryzhuk@gmail.com>
2024-06-15 11:43:08 +0300

    fix 9 indexes with pm

--- task.sql
+++ task.sql
@@ -7,19 +7,3 @@ CREATE INDEX Email ON Customers(Email);
 
 -- it helps to faster find orders
 CREATE INDEX Date ON Orders(Date);
-
--- it also need to speed up product search
-CREATE INDEX Name ON Products(Name);
-
--- orders index for id
-CREATE INDEX idx_customer_id ON Orders (CustomerID);
-
--- indexes for foreign keys in 
-CREATE INDEX idx_order_id ON OrderItems (OrderID);
-CREATE INDEX idx_product_id ON OrderItems (ProductID);
-
--- create indexes from pk
-CREATE INDEX pk_products_id ON Products(ID);
-CREATE INDEX pk_customers_id ON Customers(ID);
-CREATE INDEX pk_orders_id ON Orders(ID);
-CREATE INDEX pk_orderitems_id ON OrderItems(ID);
